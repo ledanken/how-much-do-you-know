421084f7a34680775381238e37ed51c2cdb5cd8f
ledanken <>
2023-11-23 23:40:10 +0000

    Updating the question list and create the answer list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,47 @@
-let questList = ["How many continents does the word have?",
+let questionList = ["How many continents does the world have?",
     "What is the capital city of Singapore?",
-    "14 + 7 - 3 * 5 = ?"];
+    "12 / 3 *14 + 7 - 3 * 4 = ?",
+    "The smallest country in the world:",
+    "What does 'pi' stand for?",
+    "What do you mean by flabbergasted?",
+    "What is the logarithm of 7?",
+    "The biggest country in the world",
+    "The highest mountain in the world",
+    "What is the capital city of Burkina Faso?",
+    "Which year was Johann Wolfgang von Goethe born?",
+    "Which country was the very first to come up with a 'Ministry Of Happiness'?",
+    "how many words could the very first built computer store?",
+    "Who invented the Binary System?",
+    "Where did Cacao plant originate?",
+    "12.2 * 2 * 3.5 - 17 + 5 = ?",
+    "The Pythagorean Theorem Formula",
+    "In which century was the war that lasted 30 years?",
+    "Who invented Wifi?",
+    "When was the first FIFA World Cup?"
+];
 
 
-function uponLoading() {
+let answerList = [
+    "7 Continents",
+    "Singapore",
+    "-4.71",
+    "Vatican City",
+    "3.141516",
+    "astound",
+    "0.845",
+    "Russia",
+    "Mount Everest",
+    "Ouagadougou",
+    "1749",
+    "Bhutan",
+    "64 words",
+    "Gottfried Leibniz",
+    "Latin America",
+    "63.4",
+    "L * W ",
+    "17th Century",
+    "Hedy Lamarr",
+    "1930"
+];
 
-}
 
-function questList() {
-
-}
-
-function submitAnswer() {
-
-}
-
-function reshuffleQuestions() {
-    
-}
